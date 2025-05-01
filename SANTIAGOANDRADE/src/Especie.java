dbeb280600e6fa0249cca4cfe752d0f586000362
SantiagoAndradeUDLA <santiago.andrde.bucheli@udla.edu.ec>
2025-05-01 16:41:15 -0500

    anadir Especie

---
+++ SANTIAGOANDRADE/src/Especie.java
@@ -0,0 +1,11 @@
+public class Especie {
+    public String nombre;
+    public int vida;
+    public String clase_animal;
+    public double precio;
+    public double cantidad_dispo;
+    public double cantidad_vendida;
+
+
+    public void
+}
