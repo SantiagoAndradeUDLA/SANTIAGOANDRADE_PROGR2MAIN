9215c5237a62ae7c50fdf1a1f00d4a18000d4bff
SantiagoAndradeUDLA <santiago.andrde.bucheli@udla.edu.ec>
2025-05-01 16:50:52 -0500

    ESPECIE+

--- SANTIAGOANDRADE/src/Especie.java
+++ SANTIAGOANDRADE/src/Especie.java
@@ -3,9 +3,19 @@ public class Especie {
     public int vida;
     public String clase_animal;
     public double precio;
-    public double cantidad_dispo;
-    public double cantidad_vendida;
+    public int cantidad_dispo;
+    public int cantidad_vendida;
 
 
-    public void
+    public void Especies(String nombres,int vidas,String clase_animals,double precio, int cantidad_dispos, int cantidad_vendidas){
+
+        this.nombre="Cacatua Blanca";
+        this.vida=5;
+        this.clase_animal="ave";
+        this.precio=12.5;
+        this.cantidad_dispo=2;
+        this.cantidad_vendida=5;
+
+
+    }
 }
